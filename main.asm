ac5315abe717c8c8328ff6b09a6b601c0f348ce5
TanunnasBK <31913677+jirutbk@users.noreply.github.com>
2020-04-13 21:39:04 +0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 stm8/
-  #include "mapping.inc"
+    #include "mapping.inc"
 
-  segment 'rom'
+    segment 'rom'
 Digit0	equ $88	
 Digit1	equ $F9
 Digit2	equ $4C
@@ -18,42 +18,42 @@ PB_ODR 	equ $5005
 PB_CR1 	equ $5008
 PIN_ALL equ $FF
 
-		mov PB_DDR, #PIN_ALL	;Output mode
-		mov PB_CR1, #PIN_ALL	;Push-Pull
+    mov PB_DDR, #PIN_ALL    ;Output mode
+    mov PB_CR1, #PIN_ALL    ;Push-Pull
 
 main
-		mov PB_ODR, #Digit0	;display digit
-		call delay
-		mov PB_ODR, #Digit1	
-		call delay
-		mov PB_ODR, #Digit2	
-		call delay
-		mov PB_ODR, #Digit3	
-		call delay
-		mov PB_ODR, #Digit4	
-		call delay
-		mov PB_ODR, #Digit5	
-		call delay
-		mov PB_ODR, #Digit6	
-		call delay
-		mov PB_ODR, #Digit7	
-		call delay
-		mov PB_ODR, #Digit8	
-		call delay
-		mov PB_ODR, #Digit9	
-		call delay
+    mov PB_ODR, #Digit0	    ;display digit
+    call delay
+    mov PB_ODR, #Digit1	
+    call delay
+    mov PB_ODR, #Digit2	
+    call delay
+    mov PB_ODR, #Digit3	
+    call delay
+    mov PB_ODR, #Digit4	
+    call delay
+    mov PB_ODR, #Digit5	
+    call delay
+    mov PB_ODR, #Digit6	
+    call delay
+    mov PB_ODR, #Digit7	
+    call delay
+    mov PB_ODR, #Digit8	
+    call delay
+    mov PB_ODR, #Digit9	
+    call delay
     jra main
 		
 delay:
-		ldw X, #$FF1
+    ldw X, #$FF1
 xloop		
-		ldw Y, #$55		
+    ldw Y, #$55		
 yloop
-		decw Y
-		jrne yloop	
-		decw X
-		jrne xloop
-		ret
+    decw Y
+    jrne yloop	
+    decw X
+    jrne xloop
+    ret
 		
     end
 		
